31543fb9e86ba039a970a71f591fe1ef01c0aff1
GasanaJr <gasanadj2003@gmail.com>
2024-05-10 17:09:25 +0200

    ft(flutter): add appbar, body, and bottom navigation

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Rise Farmer'),
+          backgroundColor: Colors.tealAccent,
+        ),
+        body: Text(
+          'Let us see how far we can go.',
+          style: TextStyle(fontSize: 20),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
